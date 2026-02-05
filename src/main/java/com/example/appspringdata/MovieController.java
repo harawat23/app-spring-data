b7ca88f49604f376741319c98978d7c92c032989
harawat23 <harawat@ciena.com>
2026-02-05 07:10:54 +0000

    commit

--- src/main/java/com/example/appspringdata/MovieController.java
+++ src/main/java/com/example/appspringdata/MovieController.java
@@ -1,16 +1,16 @@
 package com.example.appspringdata;
 
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
 import java.util.Optional;
-
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/movies")
@@ -23,11 +23,38 @@ public class MovieController {
 
     @GetMapping
     Iterable<Movie> getAllMovies() {
-        return movieRepository.findAll();
+        return movieRepository.findMoviesSubset();
     }
 
     @GetMapping("/{id}")
     Optional<Movie> getMovieById(@PathVariable String id) {
         return movieRepository.findById(id);      
     }
+
+    @PostMapping("/save")
+    public Movie postMethodName(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @DeleteMapping("/delete")
+    void delete(@RequestParam String movieId) {
+        movieRepository.deleteById(movieId);
+        System.out.println("Deleted movie with movieId: " + movieId);
+    }
+
+    @GetMapping("/person")
+    public Iterable<Movie> getMoviesByPerson(@RequestParam String name) {
+        return movieRepository.findMoviesByPerson(name);
+    }
+
+    @PutMapping("updateVotes")
+    public Iterable<Movie> addVoteToMovie(@RequestParam String movieId) {
+        return movieRepository.increaseVotes(movieId);
+    }
+    
+    @GetMapping("/movielist")
+    public Iterable<MovieProjection> getMovieProjection() {
+        return movieRepository.findAllMovieProjectionsBy();
+    }
+    
 }
--- src/main/java/com/example/appspringdata/PersonController.java
+++ src/main/java/com/example/appspringdata/PersonController.java
@@ -2,13 +2,9 @@ package com.example.appspringdata;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Optional;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/persons")
